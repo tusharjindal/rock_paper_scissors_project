11f8c795d9dbdbe69bb1c6950372868cd4a3c5e8
tusharjindal <tusharjindal1999@gmail.com>
2021-12-10 18:09:21 +0530

    added new buttons

--- home_page.php
+++ home_page.php
@@ -2,6 +2,8 @@
    <body> <h1>welcome to the game of stone, paper and scissor <h1> 
    <img src="pics\game_pic.png" alt="Failed to load image">
    <button type="button">Play Game</button>
+   <button type="button">Dashboard</button>
+   <button type="button">change password</button>
    </body></html?
 
    
